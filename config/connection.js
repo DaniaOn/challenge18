074865ace31a9ec2b291acaba8320c1d3b5cfbcc
danicaon <danica_escot@hotmail.com>
2023-04-15 08:11:01 -0500

    adding config, model and controllers

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-connect('mongodb://localhost/usersPosts', {
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connection('mongodb://127.0.0.1:27017/mygroceryDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-module.exports = connection;
+module.exports = mongoose.connectionconnection;
